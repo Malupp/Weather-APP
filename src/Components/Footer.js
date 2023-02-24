c47899307046d15acb5a58cef1f85d5fc86642b5
Manuel Luppino <60667104+Warlup@users.noreply.github.com>
2023-02-24 15:19:58 +0100

    update: json file for looping minicards properly and responsively

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
   return (
     <div className="flex flex-col mt-10 items-center ">
-        <h1 className="mb-8 text-xl">Previsioni della settimana</h1>
+        <h1 className="mb-2 text-xl">Previsioni della settimana</h1>
       <WeeklyCard />
     </div>
   );
--- src/Components/shared/WeeklyCard.js
+++ src/Components/shared/WeeklyCard.js
@@ -3,31 +3,53 @@ import {
   faSun,
   faTemperatureArrowUp,
   faTemperatureDown,
+  faBolt,
+  faSnowflake,
+  faSmog,
+  faCloudShowersHeavy,
+  faCloudSun,
+  faCloudRain
 } from "@fortawesome/free-solid-svg-icons";
+import cardsData from '../assets/cardsData.json'
 
-function WeeklyCard() {
+function WeeklyCard() {  
+  const icons = {
+    faSun,
+    faCloudSun,
+    faCloudRain,
+    faBolt,
+    faSnowflake,
+    faSmog,
+    faCloudShowersHeavy
+  };
   return (
-    <div>
+    <div className="flex md:flex-row flex-col flex-wrap">
       {/* single card component */}
-      <div className="border-card h-40 w-40 shadow-lg flex flex-col justify-center items-center hover:scale-125 bg-[#e0f8fb]">
+      { cardsData.map((card) => (
+        <div key={card.day} className="border-card mx-6 my-6 h-40 w-40 shadow-lg  justify-center items-center hover:scale-125 bg-[#e0f8fb]"
+        >
         <div className="text-center">
-          <h1 className="text-sm mb-4">Lun</h1>
-          <FontAwesomeIcon className="text-3xl text-orange-500" icon={faSun} />
-          <h2 className=" mt-4">Chiaro</h2>
+          <h1 className="text-sm mb-4">{card.day}</h1>
+          <FontAwesomeIcon className="text-3xl text-orange-500" icon={icons[card.icon]} />
+          <h2 className=" mt-4">{card.condition}</h2>
           <div className="flex text-2xl justify-center items-center mt-4 gap-1">
             <FontAwesomeIcon
               className="text-red-600"
               icon={faTemperatureArrowUp}
             />
-            <h2>34°</h2>
+            <h2>{card.highTemp}</h2>
             <FontAwesomeIcon
               className="text-blue-600"
               icon={faTemperatureDown}
             />
-            <h2>27°</h2>
+            <h2>{card.lowTemp}</h2>
           </div>
         </div>
       </div>
+      ))
+
+      
+      }
     </div>
   );
 }
