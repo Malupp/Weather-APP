2c5f6bc0e7fc55e6daefcb849b223b09a3ac360d
Manuel Luppino <60667104+Warlup@users.noreply.github.com>
2023-03-03 16:19:35 +0100

    fix: components paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Footer } from "/src/components/Footer";
-import { Main } from "/src/components/Main";
-import { Header } from "/src/components/Header";
+import { Footer } from "./components/Footer";
+import { Main } from "./components/Main";
+import { Header } from "./components/Header";
 import "./assets/index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
